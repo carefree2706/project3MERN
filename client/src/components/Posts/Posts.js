11e8d057c8d854186ccb06c0ce7ef2540682203e
Donald Wu <chenggeuw0512@gmail.com>
2021-05-23 17:06:59 +0930

    card creating

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
 import useStyles from './styles';
 
 
-const Posts = () => {
-    const posts = useSelector((state)=> state.posts )
+const Posts = ({ setCurrentId }) => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
-
-    console.log(posts);
-    return(
-        <>
-        <h1 className={classes.something}>POSTS</h1>
-        <Post />
-        <Post />
-
-
-        </>
-    )
-}
-
+  
+    return (
+      !posts.length ? <CircularProgress /> : (
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6} md={6}>
+              <Post post={post}  />
+            </Grid>
+          ))}
+        </Grid>
+      )
+    );
+  };            
 export default Posts;
